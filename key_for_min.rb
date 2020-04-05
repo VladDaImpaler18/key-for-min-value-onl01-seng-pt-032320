f45f700f5605146d4c961db2ef237f4bfb4f71f4
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-05 23:28:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,13 @@ def key_for_min_value(name_hash)
    smallest_value=nil
    smallest_key=nil
    name_hash.each do |key, value|
-     smallest_value = value && smallest_key = key if smallest_value == nil
-     smallest_value = value && smallest_key = key if value < smallest_value
+     if smallest_value_value==nil
+     smallest_value = value
+     smallest_key = key
+     elsif value < smallest_value
+     smallest_value = value
+     smallest_key = key if value
+     end
    end
-   return smallest
+   return smallest_key
 end
